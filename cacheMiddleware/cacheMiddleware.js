a2de525ae45d8914d46887d8ee07a7eb1554da2a
Mishakle <31828112+Mishakle@users.noreply.github.com>
2020-11-20 02:41:10 +0200

    migr+rediss

---
+++ cacheMiddleware/cacheMiddleware.js
@@ -0,0 +1,17 @@
+const redis = require('redis');
+const client = redis.createClient(6379);
+
+function cache(req, res, next) {
+    const { id } = req.params;
+    client.get(id, (err, data) => {
+        if (err) throw err;
+
+        if (data !== null) {
+        res.json(data);
+        } else {
+        next();
+        }
+    });
+}
+
+module.exports = cache;
